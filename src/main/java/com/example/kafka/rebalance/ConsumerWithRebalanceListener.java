a65cfe0c28e000f4ce8fb8eb2398e2b70ca404d3
hoo <tbnsok40@gmail.com>
2023-12-07 07:09:07 +0900

    consumer wakeup exception, close

--- src/main/java/com/example/kafka/rebalance/ConsumerWithRebalanceListener.java
+++ src/main/java/com/example/kafka/rebalance/ConsumerWithRebalanceListener.java
@@ -8,6 +8,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +22,10 @@ public class ConsumerWithRebalanceListener {
     private static KafkaConsumer<String, String> consumer;
 
     public static void main(String[] args) {
+
+        // main method 는 ShutdownThread 라는 shutdownHook 을 받는다.
+        // 즉 프로세스가 종료되면 ShutdownThread 를 실행시킴
+        Runtime.getRuntime().addShutdownHook(new ShutdownThread());
         Properties configs = new Properties();
         configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
@@ -29,11 +34,26 @@ public class ConsumerWithRebalanceListener {
 
         consumer = new KafkaConsumer<>(configs);
         consumer.subscribe(Arrays.asList(TOPIC_NAME), new RebalanceListener());
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
-            for (ConsumerRecord<String, String> record : records) {
-                logger.info("{}", record);
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+                for (ConsumerRecord<String, String> record : records) {
+                    logger.info("{}", record);
+                }
             }
+        } catch (WakeupException e) {
+            logger.warn("Wakeup Exception");
+        } finally {
+            logger.warn("consumer close");
+            consumer.close();
+        }
+    }
+
+    private static class ShutdownThread extends Thread {
+        public void run() {
+            logger.info("shutdown hook");
+            consumer.wakeup();
         }
     }
 }
